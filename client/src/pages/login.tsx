1d694ffb88b430ba1ef5aea6ca3b89d9ee29140a
Ulysses Zamora <leezam01@gmail.com>
2023-04-28 01:37:37 -0700

    Update login.tsx

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
   const submitHandler = () => {
     if (AccountSelected.currentLogin === 0) {
       axios
-        .post("/api/login", { user: userName, password })
+        .post("https://goldfish-app-wb78d.ondigitalocean.app/api/login", { user: userName, password })
         .then((res: any) => {
           if (res.status === 200) {
             localStorage.setItem("jwt", res.data);
