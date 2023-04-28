301d82076be7fb4ae64693ab8ccc199d6da4a766
Ulysses Zamora <leezam01@gmail.com>
2023-04-28 08:36:03 -0700

    Update NavBar.tsx

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import {Location, useLocation, useNavigate } from "react-router-dom";
 import NavBarStyle from "../styles/NavBarStyle.module.css"
 import jwtDecode from "jwt-decode";
-import React, { Component, useEffect, useState } from "react";
+import {useState } from "react";
 import search from "../assets/search-svgrepo-com.svg";
 import bell from "../assets/bell-svgrepo-com.svg";
 import userIcon from "../assets/user-01-svgrepo-com.svg";
@@ -40,9 +40,9 @@ const [modalOpen, setOpenModal] = useState(false);
   /* LOGIN PAGE */
   else if(pwd === "/loginpage"){
     return (
-      <nav className={NavBarStyle.nav}>
+      <nav className={NavBarStyle.loginNav}>
         <a href="/">
-        <img id="logo" src="/images/img_empowerd1.png" className={NavBarStyle.logo}/>
+        <img id="logo" src="/images/img_empowerd1.png" className={NavBarStyle.loginLogo}/>
         </a>
       </nav>
     );
@@ -50,92 +50,111 @@ const [modalOpen, setOpenModal] = useState(false);
   /*USER PROFILE*/
   else if(pwd.includes("userprofile")){
     return (
-    <div className={styles.header}>
+      <div className={styles.header}>
         {modalOpen && (
-            <UserNotifications closeModal={setOpenModal}/>
+          <UserNotifications closeModal={setOpenModal}/>
         )}
-      <div className={styles.inp_and_img}>
-        <div className={styles.empowerd1}></div>
-        <div>
-          <input
-            className={styles.inputField}
-            type="text"
-            placeholder="Search"
-          />
-          <img src={search} alt="" className={styles.svg} />
+        <div className={styles.inp_and_img}>
+          <div className={styles.empowerd1}>
+            <a href="/">
+              <img id="logo" src="/images/img_empowerd1.png" className={NavBarStyle.logo}/>
+            </a>
+          </div>
+          <div>
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="Search"
+            />
+            <img src={search} alt="" className={styles.svg} />
+          </div>
+        </div>
+          <button onClick={() => navigate("/coffeechat")} className={NavBarStyle.button}>
+            Chai Now 
+          </button>
+          <div className={styles.bell_and_prof}>
+            <img src={bell} alt="Bell icon" onClick={() => setOpenModal(true)} className={styles.bell} />
+            <img
+              src={userIcon}
+              alt="user icon"
+              className={`${styles.bell} ${styles.user}`}
+            />
         </div>
       </div>
-      <div className={styles.bell_and_prof}>
-        <img src={bell} alt="Bell icon" onClick={() => setOpenModal(true)} className={styles.bell} />
-        <img
-          src={userIcon}
-          alt="user icon"
-          className={`${styles.bell} ${styles.user}`}
-        />
-      </div>
-    </div>
-  );
+    );
   }
   /*COMPANY PROFILE*/
   else if(pwd.includes("companyprofile")){
     return (
-      <nav className={NavBarStyle.nav}>
-        <a href="/">
-        <img id="logo" src="/images/img_empowerd1.png" className={NavBarStyle.logo}/>
-        </a>
-        
-        {/* search box */}
-        <input
-          type="text"
-          placeholder="Search.."
-          className={NavBarStyle.search}
-        />
-        <button className={NavBarStyle.searchbutton}>
-          <img src="./src/assets/search.png" />
-        </button>
+      <div className={styles.header}>
+        {modalOpen && (
+          <UserNotifications closeModal={setOpenModal}/>
+        )}
+        <div className={styles.inp_and_img}>
+          <div className={styles.empowerd1}>
+            <a href="/">
+              <img id="logo" src="/images/img_empowerd1.png" className={NavBarStyle.logo}/>
+            </a>
+          </div>
+          <div>
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="Search"
+            />
+            <img src={search} alt="" className={styles.svg} />
+          </div>
+        </div>
         <button onClick={() => navigate("/coffeechat")} className={NavBarStyle.button}>
-        Chai Now 
+          Chai Now 
         </button>
-        <img
-          className={NavBarStyle.logoAvatar}
-          src="../src/assets/companyLogo.png"
-        />
-      </nav>
+        <div className={styles.bell_and_prof}>
+          <img src={bell} alt="Bell icon" onClick={() => setOpenModal(true)} className={styles.bell} />
+          <img
+            src={userIcon}
+            alt="user icon"
+            className={`${styles.bell} ${styles.user}`}
+          />
+        </div>
+      </div>
     );
   }
   /*COFFEE CHAT*/
   else if(pwd.includes("coffeechat")){
     return (
-      <nav className={NavBarStyle.nav}>
-        <a href="/">
-        <img id="logo" src="/images/img_empowerd1.png" className={NavBarStyle.logo}/>
-        </a>
-        {/*<button className={NavBarStyle.navBtn}> Join the Waitlist</button>*/}
-        
-        {/* search box */}
-        <input
-          type="text"
-          placeholder="Search.."
-          className={NavBarStyle.search}
-        />
-        <button className={NavBarStyle.searchbutton}>
-          <img src="./src/assets/search.png" />
-        </button>
+      <div className={styles.header}>
+        {modalOpen && (
+          <UserNotifications closeModal={setOpenModal}/>
+        )}
+        <div className={styles.inp_and_img}>
+          <div className={styles.empowerd1}>
+            <a href="/">
+              <img id="logo" src="/images/img_empowerd1.png" className={NavBarStyle.logo}/>
+            </a>
+          </div>
+          <div>
+            <input
+              className={styles.inputField}
+              type="text"
+              placeholder="Search"
+            />
+            <img src={search} alt="" className={styles.svg} />
+          </div>
+        </div>
         <button className={NavBarStyle.button} onClick={() => whereTo("Profile")}> 
-        Profile
+          Profile
         </button>
-        <select>
-        <img
-          className={NavBarStyle.logoAvatar}
-          src="./src/assets/avatar.png"
-        />
-        <option value="">Account</option>
-        <a><option value="">Sign Out</option></a>
-        </select>
-      </nav>
+        <div className={styles.bell_and_prof}>
+          <img src={bell} alt="Bell icon" onClick={() => setOpenModal(true)} className={styles.bell} />
+          <img
+            src={userIcon}
+            alt="user icon"
+            className={`${styles.bell} ${styles.user}`}
+          />
+        </div>
+      </div>
     );
   }
-  
 };
 
 export default NavBar;
