42eb5f3b4c750d4a4d81aac943216a0991476670
Ulysses Zamora <leezam01@gmail.com>
2023-05-01 12:10:25 -0700

    Update acceptFollower.ts

--- server/src/routes/acceptFollower.ts
+++ server/src/routes/acceptFollower.ts
@@ -15,7 +15,17 @@ acceptFollowers.put("/acceptFollowers", async (req, res) => {
   pool.query(sql, [followedId, followerId], async (err, results) => {
     if (err) {
       throw err;
+    } else {
+        sql = "INSERT INTO userfollowers (followerId, followedId, status) VALUES (?, ?, 'accepted')";
+        pool.query(sql, [followerId, followedId], async (err, results) => {
+            if(err) {
+                console.error('Error user follower into table:', err);
+                res.status(500).json({ error: 'Error user follower into table' });
+            } else {
+                console.log('Successfully updated user followers');
+                res.status(200).json({ success: 'Successfully updated user followers' });
+            }
+        })
     }
-    res.status(201).json(results);
   });
 });
