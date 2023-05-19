ca64c16857551cd653cefd4406447ef615613fc9
Ulysses Zamora <leezam01@gmail.com>
2023-05-18 20:25:59 -0700

    Update login.tsx

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -129,7 +129,7 @@ const LoginPage = () => {
             <div className={styles.mainBodyContainer}>
               <div className={styles.mainBodyContainerInner}>
                 <div className={styles.textContainer}>
-                  <h1 className={styles.empoweredTitle}>Empowered</h1>
+                  <h1 className={styles.empoweredTitle}>Empowerd</h1>
                   <h3 className={styles.inspoText}>
                     a vetted community for women & allies Empowering opportunity
                   </h3>
